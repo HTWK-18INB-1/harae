a6d929fa6ef1dd7a916f525753f4a7e0aff3ffff
zoccer99 <leon-gottschild@t-online.de>
2021-03-25 13:15:32 +0100

    updated: level procedure

--- Assets/scripts/etc/LightGuide.cs
+++ Assets/scripts/etc/LightGuide.cs
@@ -6,11 +6,13 @@ public class LightGuide : MonoBehaviour
 {
     bool[] isMove = new bool[3] {false,false,false};
     
-    public GameObject player;
+    private GameObject player;
+
+    public GameObject enemyBoss;
+    private GameObject bossParticle;
 
     public GameObject finalBook;
-    public GameObject targetOne;
-    public GameObject targetTwo;
+    public GameObject firstBook;
 
     private Light light;
     //private Halo halo;
@@ -22,60 +24,73 @@ public class LightGuide : MonoBehaviour
     Vector3 currentPosition;
     Vector3 directionOfTravel;
 
-    Vector3[] startPosition = new Vector3[3];
-    private bool[] firstStart = new bool[3] {true,true,true};
+    Vector3[] startPosition = new Vector3[2];
+    private bool[] firstStart = new bool[2] {true,true};
     private float newDistance;
     private float startDistance;
 
 
+    public float helper = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
         light = GetComponent<Light>();
-        startingColor = light.color; //light.color
-        //halo = GetComponent<Halo>();
+        startingColor = light.color;
+        bossParticle = enemyBoss.transform.GetChild(2).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
         moveToDestination(player);
-        if(Input.GetKeyDown(KeyCode.Y)){
-            isMove[0] = true;
+        lvlProcedure();
+    }
+
+
+    void riseAndShine(GameObject book,int childIndex){
+            book.transform.GetChild(childIndex).gameObject.GetComponent<ParticleSystem>().Play();
+            if(book.transform.position.y < 3){
+                Vector3 temp = book.transform.position;
+                book.transform.position += new Vector3(0,1*Time.deltaTime,0);
+            }
+
         }
 
+    void lvlProcedure(){
+        isMove[0] = true;
+
         if(isMove[0]){
-            //isMove[1]=moveToDestination(targetOne);
             if(firstStart[0]){
                 startPosition[0] = this.transform.position;
             }
             firstStart[0] = false;
-            changeColor(targetOne,startPosition[0]);
+            changeColor(firstBook,startPosition[0]);
+            if(helper>5){
+                riseAndShine(firstBook,5);
+            }
+            
         }
 
+        //TODO : wenn erstes Item gefunden dann -> 
         if(isMove[1]){
             isMove[0]=false;
-            //isMove[2]=moveToDestination(targetTwo);
             if(firstStart[1]){
                 startPosition[1] = this.transform.position;
             }
             firstStart[1] = false;
-            changeColor(targetTwo,startPosition[1]);
-        }
-
-        if(isMove[2]){
-            isMove[1]=false;
-            //moveToDestination(finalBook);
-            if(firstStart[2]){
-                startPosition[2] = this.transform.position;
+            changeColor(finalBook,startPosition[1]);
+            if(helper>10){
+                riseAndShine(finalBook,3);
             }
-            firstStart[2] = false;
-            changeColor(finalBook,startPosition[2]);
         }
 
+        //TODO : wenn zweites Item gefunden dann -> 
+
+            enemyAppear();
         
-    
-        
+
     }
 
     bool moveToDestination(GameObject destination){
@@ -106,12 +121,18 @@ public class LightGuide : MonoBehaviour
         startDistance = Vector3.Distance(startPositionf,targetPosition);
 
         float ratio = 1-(newDistance/startDistance);
-        Debug.Log("RATIO -> " + ratio);
-        Debug.Log("newDistance -> " + newDistance);
-        Debug.Log("startDistance -> " + startDistance);
-
         light.color = Color.Lerp(startingColor,Color.red,ratio);
+    }
 
-        Debug.Log(ratio);
+    void enemyAppear(){
+        if(enemyBoss.transform.position.y<0){
+            bossParticle.GetComponent<ParticleSystem>().Play();
+            Vector3 temp = bossParticle.transform.position;
+            enemyBoss.transform.position += new Vector3(0,3*Time.deltaTime,0);
+                bossParticle.transform.position = temp;
+        }
+
+        
+        
     }
 }
